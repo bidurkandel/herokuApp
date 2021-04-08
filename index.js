7a5eeb868bedd239e7cc98ef647e9c18129cbcc4
daveKidd <david.kidd@lambdaschool.com>
2021-04-08 10:51:19 -0600

    push

---
+++ index.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use("/api/",(_,res)=>{
+    res.json({data:"API is accounted for"})
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is alive on port ${port}`)
+})
+
+// console.log("Its alive!!!!!!")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)//mac USER
+// console.log(process.env.PORT)
+// console.log(process.env.FOOD)
+// console.log(process.env.REALLY)
+// console.log(process.env.GREET)
